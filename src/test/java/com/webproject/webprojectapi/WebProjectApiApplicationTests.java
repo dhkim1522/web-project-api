547724a2cc5352c3fe97ada1678113be300ffa86
dhkim1522 <dhkim1522@naver.com>
2022-07-04 11:02:53 +0900

    project create

---
+++ src/test/java/com/webproject/webprojectapi/WebProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webproject.webprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
