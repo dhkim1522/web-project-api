4f9b3810bd7724ef9423ce00d886142c9c94d38c
dhkim1522 <dhkim1522@naver.com>
2022-08-05 11:33:58 +0900

    Spring Cloud Gateway 구현을 위한 설정 추가

--- src/main/java/com/webproject/webprojectapi/config/WebConfig.java
+++ src/main/java/com/webproject/webprojectapi/config/WebConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-
     @Override
     // Cross Origin Allow all
     public void addCorsMappings(CorsRegistry registry) {
